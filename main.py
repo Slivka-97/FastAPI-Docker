71b0baf292d018dc47f246059d23b092bb9dc8f0
SlivkaIgor <Slivka-97@mail.ru>
2022-07-16 15:05:52 +0300

    add Dockerfile

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from model import Model
 app = FastAPI()
 
 
-@app.get('/api/{age:int}')
-async def main(age: int, name: str = 'dd'):
-    return {'message': f'Hello {age}--{name}'}
+@app.get('/')
+async def main():
+    return {'Hello'}
 
 
 @app.post('/api/post/')
