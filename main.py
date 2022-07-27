ae0a9e33364b0d9f0053b1343cf52efb154cb794
Slivka Igor <Slivka.Igor@infotecs-nt>
2022-07-27 10:39:07 +0300

    add method

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from model import Model
 app = FastAPI()
 
 
+@app.get('')
+async def main():
+    return {'message': f'Hello World'}
+
+
 @app.get('/api/{age:int}')
 async def main(age: int, name: str = 'dd'):
     return {'message': f'Hello {age}--{name}'}
